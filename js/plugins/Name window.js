1009ccb5ee48304032bb85782c29d5dbf46c1432
TG-angel <tgmakescoolvidshehe@gmail.com>
2025-09-02 16:29:07 +0100

    hfdhfhfj

--- js/plugins/Name window.js
+++
@@ -1,9 +0,0 @@
-Scene_Name.prototype.editWindowRect = function() {
-    const inputWindowHeight = this.calcWindowHeight(9, true);
-    const padding = $gameSystem.windowPadding();
-    const ww = 500;
-    const wh = 150
-    const wx = (Graphics.boxWidth - ww) / 2;
-    const wy = -50
-    return new Rectangle(wx, wy, ww, wh);
-};
